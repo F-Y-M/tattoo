86eb24ca413327adaae6af0bc242a2069f84f8fa
FYM <jan.juan@misena.edu.co>
2020-04-17 20:37:14 -0500

    Updates

---
+++ gallery/imgs.js
@@ -0,0 +1,422 @@
+import React from 'react';
+//imagenes
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+import img5 from './img/img5.jpg';
+import img6 from './img/img6.jpg';
+import img7 from './img/img7.jpg';
+import img8 from './img/img8.jpg';
+import img9 from './img/img9.jpg';
+import img10 from './img/img10.jpg';
+import img11 from './img/img11.jpg';
+import img12 from './img/img12.jpg';
+import img13 from './img/img13.jpg';
+import img14 from './img/img14.jpg';
+import img15 from './img/img15.jpg';
+import img16 from './img/img16.jpg';
+import img17 from './img/img17.jpg';
+import img18 from './img/img18.jpg';
+import img19 from './img/img19.jpg';
+import img20 from './img/img20.jpg';
+import img21 from './img/img21.jpg';
+import img22 from './img/img22.jpg';
+import img23 from './img/img23.jpg';
+import img24 from './img/img24.jpg';
+import img25 from './img/img25.jpg';
+import img26 from './img/img26.jpg';
+import img27 from './img/img27.jpg';
+import img28 from './img/img28.jpg';
+import img29 from './img/img29.jpg';
+import img30 from './img/img30.jpg';
+import img31 from './img/img31.jpg';
+import img32 from './img/img32.jpg';
+import img33 from './img/img33.jpg';
+import img34 from './img/img34.jpg';
+import img35 from './img/img35.jpg';
+import img36 from './img/img36.jpg';
+import img37 from './img/img37.jpg';
+import img38 from './img/img38.jpg';
+import img39 from './img/img39.jpg';
+import img40 from './img/img40.jpg';
+import img41 from './img/img41.jpg';
+import img42 from './img/img42.jpg';
+import img43 from './img/img43.jpg';
+import img44 from './img/img44.jpg';
+import img45 from './img/img45.jpg';
+import img46 from './img/img46.jpg';
+import img47 from './img/img47.jpg';
+import img48 from './img/img48.jpg';
+import img49 from './img/img49.jpg';
+import img50 from './img/img50.jpg';
+import img51 from './img/img51.jpg';
+import img52 from './img/img52.jpg';
+import img53 from './img/img53.jpg';
+import img54 from './img/img54.jpg';
+import img55 from './img/img55.jpg';
+import img56 from './img/img56.jpg';
+import img57 from './img/img57.jpg';
+import img58 from './img/img58.jpg';
+import img59 from './img/img59.jpg';
+
+const imgs = 
+[
+    {
+        img : img1,
+        cols : 1,
+        rows : 2,
+        index : 1
+    },
+    {
+        img : img2,
+        cols : 2,
+        rows : 2,
+        index : 2
+    },
+    {
+        img : img3,
+        cols : 1,
+        rows : 2,
+        index : 3
+    },
+    {
+        img : img4,
+        cols : 2,
+        rows : 2,
+        index : 4
+    },
+    {
+        img : img5,
+        cols : 2,
+        rows : 2,
+        index : 5
+    },
+    {
+        img : img6,
+        cols : 1,
+        rows : 2,
+        index : 6
+    },
+    {
+        img : img7,
+        cols : 1,
+        rows : 2,
+        index : 7
+    },
+    {
+        img : img8,
+        cols : 1,
+        rows : 2,
+        index : 8
+    },
+    {
+        img : img9,
+        cols : 1,
+        rows : 2,
+        index : 9
+    },
+    {
+        img : img10,
+        cols : 2,
+        rows : 2,
+        index : 10
+    },
+    {
+        img : img11,
+        cols : 1,
+        rows : 2,
+        index : 11
+    },
+    {
+        img : img12,
+        cols : 1,
+        rows : 2,
+        index : 12
+    },
+    {
+        img : img13,
+        cols : 2,
+        rows : 2,
+        index : 13
+    },
+    {
+        img : img14,
+        cols : 3,
+        rows : 2,
+        index : 14
+    },
+    {
+        img : img15,
+        cols : 1,
+        rows : 2,
+        index : 15
+    },
+    {
+        img : img16,
+        cols : 2,
+        rows : 2,
+        index : 16
+    },
+    {
+        img : img17,
+        cols : 1,
+        rows : 2,
+        index : 17
+    },
+    {
+        img : img18,
+        cols : 1,
+        rows : 2,
+        index : 18
+    },
+    {
+        img : img19,
+        cols : 1,
+        rows : 2,
+        index : 19
+    },
+    {
+        img : img20,
+        cols : 3,
+        rows : 2,
+        index : 20
+    },
+    {
+        img : img21,
+        cols : 2,
+        rows : 2,
+        index : 21
+    },
+    {
+        img : img22,
+        cols : 1,
+        rows : 2,
+        index : 22
+    },
+    {
+        img : img23,
+        cols : 2,
+        rows : 2,
+        index : 23
+    },
+    {
+        img : img24,
+        cols : 1,
+        rows : 2,
+        index : 24
+    },
+    {
+        img : img25,
+        cols : 1,
+        rows : 2,
+        index : 25
+    },
+    {
+        img : img26,
+        cols : 1,
+        rows : 2,
+        index : 26
+    },
+    {
+        img : img27,
+        cols : 2,
+        rows : 2,
+        index : 27
+    },
+    {
+        img : img28,
+        cols : 1,
+        rows : 2,
+        index : 28
+    },
+    {
+        img : img29,
+        cols : 2,
+        rows : 2,
+        index : 29
+    },
+    {
+        img : img30,
+        cols : 2,
+        rows : 2,
+        index : 30
+    },
+    {
+        img : img31,
+        cols : 1,
+        rows : 2,
+        index : 31
+    },
+    {
+        img : img33,
+        cols : 2,
+        rows : 2,
+        index : 33
+    },
+    {
+        img : img32,
+        cols : 3,
+        rows : 2,
+        index : 32
+    },
+    {
+        img : img34,
+        cols : 2,
+        rows : 2,
+        index : 34
+    },
+    {
+        img : img35,
+        cols : 2,
+        rows : 2,
+        index : 35
+    },
+    {
+        img : img36,
+        cols : 1,
+        rows : 2,
+        index : 36
+    },
+    {
+        img : img37,
+        cols : 3,
+        rows : 2,
+        index : 37
+    },
+    {
+        img : img38,
+        cols : 2,
+        rows : 2,
+        index : 38
+    },
+    {
+        img : img39,
+        cols : 3,
+        rows : 2,
+        index : 39
+    },
+    {
+        img : img40,
+        cols : 1,
+        rows : 2,
+        index : 40
+    },
+    {
+        img : img41,
+        cols : 1,
+        rows : 2,
+        index : 41
+    },
+    {
+        img : img42,
+        cols : 3,
+        rows : 2,
+        index : 42
+    },
+    {
+        img : img43,
+        cols : 3,
+        rows : 2,
+        index : 43
+    },
+    {
+        img : img44,
+        cols : 2,
+        rows : 2,
+        index : 44
+    },
+    {
+        img : img45,
+        cols : 2,
+        rows : 2,
+        index : 45
+    },
+    {
+        img : img46,
+        cols : 2,
+        rows : 2,
+        index : 46
+    },
+    {
+        img : img47,
+        cols : 2,
+        rows : 2,
+        index : 47
+    },
+    {
+        img : img48,
+        cols : 2,
+        rows : 2,
+        index : 48
+    },
+    {
+        img : img49,
+        cols : 3,
+        rows : 2,
+        index : 49
+    },
+    {
+        img : img50,
+        cols : 1,
+        rows : 2,
+        index : 50
+    },
+    {
+        img : img51,
+        cols : 2,
+        rows : 2,
+        index : 51
+    },
+    {
+        img : img52,
+        cols : 2,
+        rows : 2,
+        index : 52
+    },
+    {
+        img : img53,
+        cols : 1,
+        rows : 2,
+        index : 53
+    },
+    {
+        img : img54,
+        cols : 2,
+        rows : 2,
+        index : 54
+    },
+    {
+        img : img55,
+        cols : 3,
+        rows : 2,
+        index : 55
+    },
+    {
+        img : img56,
+        cols : 3,
+        rows : 2,
+        index : 56
+    },
+    {
+        img : img57,
+        cols : 2,
+        rows : 2,
+        index : 57
+    },
+    {
+        img : img58,
+        cols : 3,
+        rows : 2,
+        index : 58
+    },
+    {
+        img : img59,
+        cols : 3,
+        rows : 2,
+        index : 59
+    },
+]
+
+
+export default imgs;
---
+++ precache-manifest.d43fcfde6a4d34c083a7a1aa62d6874a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "156dbb04af6e215920e4037cef42bbbe",
+    "url": "/tattoo/index.html"
+  },
+  {
+    "revision": "78cabdda0109baeb3fc0",
+    "url": "/tattoo/static/css/main.0832c55c.chunk.css"
+  },
+  {
+    "revision": "a3ff258fe589d25f7c7e",
+    "url": "/tattoo/static/js/2.48e5911e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tattoo/static/js/2.48e5911e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78cabdda0109baeb3fc0",
+    "url": "/tattoo/static/js/main.df39ec33.chunk.js"
+  },
+  {
+    "revision": "8caf435f933500c1481d",
+    "url": "/tattoo/static/js/runtime-main.23915efd.js"
+  },
+  {
+    "revision": "32230c39b6cd4acda4c6de195a1d5511",
+    "url": "/tattoo/static/media/BlackFlag.32230c39.ttf"
+  },
+  {
+    "revision": "e4e0d9566f740750f4c6135f80827c45",
+    "url": "/tattoo/static/media/Seagram.e4e0d956.ttf"
+  },
+  {
+    "revision": "78cdc59a177225bc86b054720f0b425a",
+    "url": "/tattoo/static/media/Vintage-punk.78cdc59a.ttf"
+  },
+  {
+    "revision": "2ca7762aac38ddd306a81f68829db6c1",
+    "url": "/tattoo/static/media/img47.2ca7762a.jpg"
+  }
+]);
