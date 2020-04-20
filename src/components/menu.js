46bf22c595dc1ffcd92cf8ec40608fb3d6b44237
FYM <jan.juan@misena.edu.co>
2020-04-20 17:30:24 -0500

    building...

--- src/components/menu.js
+++ src/components/menu.js
@@ -3,64 +3,54 @@ import ToggleButton from './toggleButton';
 // css
 import './menu.css'
 class Menu extends Component{
+
   state ={
       toggle : true
   }
-
-  toggleMenu = () =>{
-    if (this.state.toggle === true) {
-        return menu;
-    }else{
-        return null;
-    }
-  }
-
+   
     render(){
         return(
             <div className="nav">
                 <ToggleButton/>
-                {this.toggleMenu} 
+                <ul className="menu">
+                    <li className="elemento">
+                        <p>
+                            <a href="#home">
+                                home
+                            </a>
+                        </p>
+                    </li>
+                    <li className="elemento">
+                        <p>
+                            <a href="#galeria">
+                                galeria
+                            </a>
+                        </p>
+                    </li>
+                    <li className="elemento">
+                        <p>
+                            <a href="#citas">
+                                citas
+                            </a>
+                        </p>
+                    </li>
+                    <li className="elemento">
+                        <p>
+                            <a href="#conocenos" className="contact">
+                                conocenos
+                            </a>
+                        </p>
+                    </li>
+                    <li className="contact">
+                        <span>
+                            +57 3118623161
+                        </span>    
+                    </li>
+                </ul>
+
             </div>
         );
     }
 }
-
-const menu = 
-    <ul className="menu" >
-    <li className="elemento">
-        <p>
-            <a href="#home">
-                home
-            </a>
-        </p>
-    </li>
-    <li className="elemento">
-        <p>
-            <a href="#galeria">
-                galeria
-            </a>
-        </p>
-    </li>
-    <li className="elemento">
-        <p>
-            <a href="#citas">
-                citas
-            </a>
-        </p>
-    </li>
-    <li className="elemento">
-        <p>
-            <a href="#conocenos" className="contact">
-                conocenos
-            </a>
-        </p>
-    </li>
-    <li className="contact">
-        <span>
-            +57 3118623161
-        </span>    
-    </li>
-    </ul>
-;
 
 export default Menu;
--- src/components/toggleButton.js
+++ src/components/toggleButton.js
@@ -3,14 +3,7 @@ import React, { Component } from 'react';
 import './toggleButton.css';
 
 export default class ToggleButton extends Component {
-    componentDidUpdate(){
-        const svg = 
-        this.state.toggle
-        ? <svg aria-hidden='true'focusable='false'data-prefix='fas' data-icon='bars'class='svg-inline--fa fa-bars fa-w-14' role='img' xmlns='http://www.w3.org/2000/svg'viewBox='0 0 448 512'><path fill='currentColor'd='M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z'></path></svg>
-        : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
-
-    }
-
+ 
     state = {
         open : false,
         toggle : true
@@ -18,11 +11,13 @@ export default class ToggleButton extends Component {
 
     onClick = () =>{
         this.setState({open: !this.state.open,toggle: !this.state.toggle})
+        console.log(this.state)
     }
+
     render(){
 
         return(
-            <button className="btn" onClick={this.props.onClick}> 
+            <button className="btn" style={this.state.toggle ? close : open } onClick={this.onClick}> 
                 <Svg toggle={this.state.toggle}/>
             </button>
 
@@ -30,12 +25,29 @@ export default class ToggleButton extends Component {
     }
 }
 
+const open = { 
+    float:'right',
+    background: 'red'
+}
+
+const close = {
+    float:'left'
+}
 class Svg extends Component{
+
+    static getDerivedStateFromProps(nextProps, state) {
+        return nextProps.toggle === state.toggle
+          ? {}
+          : {toggle: nextProps.toggle}
+      }
+
  state ={
      toggle: this.props.toggle
  }   
 render(){
+    console.log(this.props)
     return(
+        
         this.state.toggle
         ? <svg aria-hidden='true'focusable='false'data-prefix='fas' data-icon='bars'class='svg-inline--fa fa-bars fa-w-14' role='img' xmlns='http://www.w3.org/2000/svg'viewBox='0 0 448 512'><path fill='currentColor'd='M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z'></path></svg>
         : <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"/></svg>
